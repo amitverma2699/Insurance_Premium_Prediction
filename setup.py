d3abecac40e6b2b9fecb531ee3615b67ef98974e
amitverma2699 <amit99bhama@gmail.com>
2024-10-22 13:10:01 +0530

    Add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+from typing import List
+
+setup(
+    name='InsurancePremiumPrediction',
+    version='0.0.2',
+    author='Amit Verma',
+    author_email='amit99bhama@gmail.com',
+    packages=find_packages()
+)
