473682a07d01df93dff4768223d248489788a714
amitverma2699 <amit99bhama@gmail.com>
2025-03-18 22:16:44 +0530

    Update files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="insurance_prediction",
+    version='0.0.1',
+    author='Amit Verma',
+    author_email="amit99bhama@gmail.com",
+    packages=find_packages()
+)
